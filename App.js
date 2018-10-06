39e86f3c95a9bb3794fb188b30214ca690f79342
georgemmp <machadogeorge.p@gmail.com>
2018-10-05 21:59:58 -0300

    Configuração do redux

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 import Route from './src/route/Route';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Route />
+      <Provider store={createStore(reducers)}>
+        <Route />
+      </Provider>
     );
   }
 }
