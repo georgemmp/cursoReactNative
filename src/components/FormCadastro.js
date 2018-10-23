9b77147cc05923b62131705059c936b60962455c
georgemmp <machadogeorge.p@gmail.com>
2018-10-23 11:40:33 -0300

    FormCadastro - Alterando os estados do componente via ActionCreators - parte 1

--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { View, TextInput, Button } from 'react-native';
 import { connect } from 'react-redux';
+import { modificaEmail, modificaSenha } from '../actions/AutenticacaoActions';
 
 const formCadastro = props => (
     <View style={{ flex: 1, padding: 10 }}>
         <View style={{ flex: 2, justifyContent: 'center' }}>
             <TextInput value={props.nome} placeholder="Nome" style={{ fontSize: 20, height: 45 }} />
-            <TextInput value={props.email} placeholder="Email" style={{ fontSize: 20, height: 45 }} />
-            <TextInput value={props.senha} placeholder="Senha" style={{ fontSize: 20, height: 45 }} />
+            <TextInput value={props.email} placeholder="Email" style={{ fontSize: 20, height: 45 }} 
+                onChangeText={texto => props.modificaEmail(texto)} />
+            <TextInput value={props.senha} placeholder="Senha" style={{ fontSize: 20, height: 45 }} 
+                onChangeText={texto => props.modificaSenha(texto)}/>
         </View>
 
         <View style={{ flex: 1 }}>
@@ -25,4 +28,4 @@ const mapStateToProps = state => (
 );
 
 
-export default connect(mapStateToProps, null)(formCadastro);
+export default connect(mapStateToProps, { modificaEmail, modificaSenha })(formCadastro);
