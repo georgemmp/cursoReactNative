97108cbbbd16f5014447a67771331720a688bced
georgemmp <machadogeorge.p@gmail.com>
2018-11-16 11:57:34 -0200

    Credenciais

--- App.js
+++ App.js
@@ -9,12 +9,6 @@ import reducers from './src/reducers';
 export default class App extends React.Component {
   componentWillMount() {
     const config = {
-      apiKey: 'AIzaSyCfi-ndgksEZI3UNO4ovDj_VKvQdViz-Eg',
-      authDomain: 'whatsapp-clone-dc72a.firebaseapp.com',
-      databaseURL: 'https://whatsapp-clone-dc72a.firebaseio.com',
-      projectId: 'whatsapp-clone-dc72a',
-      storageBucket: 'whatsapp-clone-dc72a.appspot.com',
-      messagingSenderId: '89301573398'
     };
     firebase.initializeApp(config);  
   }
