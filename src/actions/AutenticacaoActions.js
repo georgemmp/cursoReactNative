382d9a801c53412d60e2ed28bae925e5834b8363
georgemmp <machadogeorge.p@gmail.com>
2018-12-21 13:29:47 -0200

    Gravação do nome do usuário no banco de dados

--- src/actions/AutenticacaoActions.js
+++ src/actions/AutenticacaoActions.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
+import b64 from 'base-64';
 
 const modificaEmail = (texto) => ({
     type: 'modifica_email',
@@ -19,7 +20,12 @@ const modificaNome = (texto) => ({
 const cadastrarUsuario = ({ nome, email, senha }) => {
     return dispatch => {
         firebase.auth().createUserWithEmailAndPassword(email, senha)
-            .then(user => cadastraUsuarioSucesso(dispatch))
+            .then(user => {
+                const emailB64 = b64.encode(email);
+                firebase.database().ref(`/contatos/${emailB64}`)
+                    .push({ nome })
+                    .then(value => cadastraUsuarioSucesso(dispatch));
+            })
             .catch(error => cadastraUsuarioErro(error, dispatch));
     };
 };
